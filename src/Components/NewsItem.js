fbc368249f8392caab184fb1d8a4e3f57f443b27
MadhavNyati1606 <madhavnyati1606@gmail.com>
2022-11-21 23:51:15 +0530

    Added dates and author to NewsItem

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let {title, description, imageUrl, newsUrl, author, date} = this.props;
     return (
       <div>
        <div className="card my-4" style={{width: "18rem" , height: "auto"}}>
@@ -10,6 +10,7 @@ export class NewsItem extends Component {
             <div className="card-body">
               <h5 className="card-title">{title}...</h5>
               <p className="card-text">{description}...</p>
+              <p className="card-text"><small className='text-muted'>By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
               <a href={newsUrl} target = "_ blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
